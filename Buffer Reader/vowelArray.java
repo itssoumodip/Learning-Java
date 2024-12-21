fa9e14ac8c35765189c275f45f265ad0dee43532
Soumodip Das <soumodipddas2005@gmail.com>
2024-12-21 17:14:28 +0000

    vowel list

--- Buffer Reader/vowelArray.java
+++ Buffer Reader/vowelArray.java
@@ -1,35 +1,41 @@
-
 import java.io.*;
-
-class vowelJava {
-
-    public static void main() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the String : ");
-        String x = br.readLine();
-        char v[] = new char[x.length() - 1];
-        char c[] = new char[x.length() - 1];
-        int i, vo = 0, co = 0;
-        for (i = 0; i < x.length(); i++) {
-            char p = x.charAt(i);
-            switch (p) {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                case 'A':
-                case 'E':
-                case 'I':
-                case 'O':
-                case 'U':
-                    v[vo] = p;
-                    vo++;
-                    break;
-                default:
-                    c[co]=p;
-                    co++;
-            }
-        }
+public class vowelArray
+{
+    public void check()throws IOException
+    {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       System.out.print("Enter the String = ");
+       String x=br.readLine();
+       char v[]=new char[x.length()-1];
+       char c[]=new char[x.length()-1];
+       int i,vo=0,co=0;
+       for(i=0;i<x.length();i++)
+       {
+         char p=x.charAt(i);
+       switch(p)
+       {
+          case 'a':case 'e':case 'i':case 'o':case 'u':
+          case 'A':case 'E':case 'I':case 'O':case 'U':
+            v[vo]=p;
+            vo++;
+            break;
+          default:
+              c[co]=p;
+              co++;
+       }
+     }
+       System.out.print("Vowel List = ");
+       for(i=0;i<v.length;i++)
+         System.out.print(v[i]);
+       System.out.println();
+       System.out.print("Consonant List = ");
+       for(i=0;i<c.length;i++)
+         System.out.print(c[i]);
+       
+    }
+    public static void main(String[] args) throws IOException
+    {
+        vowelArray ob =new vowelArray();
+        ob.check();
     }
 }
