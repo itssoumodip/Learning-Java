2e516f5b72aa0ef33eb554e7c066bec1fabd2dba
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-04 04:54:04 +0000

    learning..

---
+++ 5. exception Handling/Ex6_Throw.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Ex6_Throw
+{
+    public static void main(String[] soumodip) {
+        while (true)
+        {
+            try
+            {
+                Scanner ob = new Scanner (System.in);
+                System.out.print("Enter the No. : ");
+                int x = ob.nextInt();
+                if (x<10)
+                    throw new NullPointerException();
+                else 
+                {
+                    System.out.print("Valid Input");
+                    break;
+                }
+            }
+            catch(NullPointerException e)
+            {
+                System.out.println("Invalid input no. must be greater then equals to 10");
+            }
+        }
+
+    }
+}
