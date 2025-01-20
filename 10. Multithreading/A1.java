14e69cea548ed51d1a61b5f29344cc877c9d9c6d
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-20 16:22:51 +0000

    about thread

---
+++ 10. Multithreading/A1.java
@@ -0,0 +1,9 @@
+class A1 
+{
+    public static void main(String soumo[])
+    {
+        Thread ob = new Thread();
+        System.out.println(ob.currentThread());
+        System.out.println(ob.currentThread());
+    }
+}
