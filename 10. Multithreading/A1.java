f28e8b0eb0402a269180aa4199154064b5ee973c
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-20 17:58:24 +0000

    about the thread

--- 10. Multithreading/A1.java
+++ 10. Multithreading/A1.java
@@ -4,6 +4,13 @@ class A1
     {
         Thread ob = new Thread();
         System.out.println(ob.currentThread());
-        System.out.println(ob.currentThread());
+        ob.setName("soumodip");
+        System.out.println(ob);
+        ob.setPriority(10);
+        int x = ob.getPriority();
+        System.out.println(x);
+        String y = ob.getName();
+        System.out.println(y);
+
     }
 }
