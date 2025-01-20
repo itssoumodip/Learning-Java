33d59a936766de347261a66660978ccb5a0daf9b
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-20 18:37:47 +0000

    about thread

---
+++ 10. Multithreading/B3.java
@@ -0,0 +1,33 @@
+class B2 implements Runnable
+{
+    public void run()
+    {
+        for (int i=1; i<11; i++)
+        {
+            try 
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){}
+            System.out.println("Run Method "+i);
+        }
+    }
+}
+class B3 
+{
+    public static void main (String soumo[])
+    {
+        B2 ob = new B2();
+        Thread t = new Thread(ob);
+        t.start();
+        for (int i=1; i<11; i++)
+        {
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e) {}
+            System.out.println("Main Executed "+i);
+        }
+    }
+}
