37d5c3d62cefc78799fa121a34363a300f2058d7
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-26 15:49:42 +0000

    about default constractor

--- 6. constructor/calc.java
+++ 6. constructor/calc.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 public class calc 
 {
-    
+    public int len, br, ar, pr;
+    public calc() // Default constractor
+    {
+        ar = 0;
+        pr = 0;
+    } 
+    public calc (int x, int y) 
 }
