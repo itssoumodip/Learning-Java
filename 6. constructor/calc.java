152f0335e0b158f710b7d116f47adfc32f931c3b
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-28 04:52:16 +0000

    fixing the errors

--- 6. constructor/calc.java
+++ 6. constructor/calc.java
@@ -1,24 +1,29 @@
+import java.util.Scanner;
 public class calc 
 {
+    
     public int len, br, ar, pr;
     public calc() // Default constractor
     {
+        System.out.println("!THIS IS DEFAULT CONSTRACTOR!");
         ar = 0;
         pr = 0;
     } 
     public calc (int x, int y) // parameterzied constractor
     {
+        System.out.println("!THIS IS PARAMETERIZED CONSTRACTOR!");
         ar = x * y;
         pr = 2*(x+y);
         System.out.println("Area = "+ar+", Perimeter = "+pr);
     }
     public calc (calc obj)
     {
+        System.out.println("!THIS IS COPY CONSTRACTOR!");
         int a = obj.len;
         int b = obj.br;
         ar = a * b;
         br = 2*(a+b);
-        System.out.println("Area is "+ar+"Perimeter is "+pr);
+        System.out.println("Area is "+ar+" Perimeter is "+pr);
     }
     public static void main(String soumo[])
     {
@@ -27,9 +32,9 @@ public class calc
         System.out.print("Enter the Length of Rect. : ");
         ob1.len = ob.nextInt(); 
         System.out.print("Enter the Breadth of Rect. : ");
-        ob2.br = ob.nextInt();
+        ob1.br = ob.nextInt();
         calc ob2 = new calc (ob1.len, ob1.br); // Parameterzied Constractor Calling
-        calc ob3 = new calc (ob); //copy constractor calling
-        calc ob4 = new calc (ob2); //Once again copy constactor calling
+        calc ob3 = new calc (ob1); //copy constractor calling
+        //calc ob4 = new calc (ob2); //Once again copy constactor calling
     }
 }
