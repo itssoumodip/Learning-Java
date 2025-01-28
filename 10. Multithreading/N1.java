2f68ea05adbef5d9d3b8e76c1a011ae57cb74da3
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-28 05:43:28 +0000

    about Threading

--- 10. Multithreading/N1.java
+++ 10. Multithreading/N1.java
@@ -1,15 +1,31 @@
-class N1 implements Runnable
+class N0 implements Runnable
 {
     Thread t;
-    N1()
+    N0()
     {
         t = new Thread(this, "Soumodip");
         t.start();
     }
     public void run()
     {
-        String s = t.getName();
-        System.out.println("Thread Name : "+s+" value : "+i);
+        for (int i=0; i<11; i++)
+        {
+            String s = t.getName();
+            System.out.println("Thread Name : "+s+" value : "+i);
+        }
     }
-    
+}
+class N1 
+{
+    public static void main(String soumo[])
+    {
+        N0 ob = new N0();
+        for(int i=0; i<11; i++)
+        {
+            String s1 = Thread.currentThread().getName();
+            // System.out.println(Thread.currentThread());
+            // System.out.println(Thread.currentThread().getName());
+            System.out.println("Main Thread Name : "+s1+" Value : "+i);
+        }
+    }
 }
