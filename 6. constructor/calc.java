944c4456607aa3842f59a2dd0a2c2d3954ba6773
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-28 04:43:07 +0000

    parameterized constractor

--- 6. constructor/calc.java
+++ 6. constructor/calc.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class calc 
 {
     public int len, br, ar, pr;
@@ -7,5 +6,29 @@ public class calc
         ar = 0;
         pr = 0;
     } 
-    public calc (int x, int y) 
+    public calc (int x, int y) // parameterzied constractor
+    {
+        ar = x * y;
+        pr = 2*(x+y);
+        System.out.println("Area = "+ar+", Perimeter = "+pr);
+    }
+    public calc (calc obj)
+    {
+        int a = obj.len;
+        int b = obj.br;
+        ar = a * b;
+        br = 2*(a+b);
+        System.out.println("Area is "+ar+"Perimeter is "+pr);
+    }
+    public static void main(String soumo[])
+    {
+        Scanner ob = new Scanner(System.in);
+        calc ob1 = new calc(); // Default Constractor Calling
+        System.out.print("Enter the Length of Rect. : ");
+        ob1.len = ob.nextInt(); 
+        System.out.print("Enter the Breadth of Rect. : ");
+        ob2.br = ob.nextInt();
+        calc ob2 = new Calc (ob1.len, ob1.br); // Parameterzied Constractor Calling
+        
+    }
 }
