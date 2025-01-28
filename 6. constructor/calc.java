e7d2ab58cd9199df28a6cffdfae90cad045042a3
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-28 05:06:52 +0000

    constractor

--- 6. constructor/calc.java
+++ 6. constructor/calc.java
@@ -22,7 +22,7 @@ public class calc
         int a = obj.len;
         int b = obj.br;
         ar = a * b;
-        br = 2*(a+b);
+        pr = 2*(a+b);
         System.out.println("Area is "+ar+" Perimeter is "+pr);
     }
     public static void main(String soumo[])
@@ -35,6 +35,6 @@ public class calc
         ob1.br = ob.nextInt();
         calc ob2 = new calc (ob1.len, ob1.br); // Parameterzied Constractor Calling
         calc ob3 = new calc (ob1); //copy constractor calling
-        //calc ob4 = new calc (ob2); //Once again copy constactor calling
+        calc ob4 = new calc (ob1); //Once again copy constactor calling
     }
 }
