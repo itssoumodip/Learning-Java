77021db411bd1e6b4f1ffc161a10b966d991a1ab
itssoumodip <soumodipddas2005@gmail.com>
2025-01-29 17:30:41 +0530

    about applet

--- 11. Applet/Part 1/A1.java
+++ 11. Applet/Part 1/A1.java
@@ -1,6 +1,7 @@
 import java.applet.*;
 import java.awt.*;
 
+// <applet code = A1 width = 500 height = 500></applet>
 public class A1 extends Applet
 {
     Font ob, ob1, ob2;
@@ -9,11 +10,19 @@ public class A1 extends Applet
     {
         setBackground(Color.black);
         setForeground(Color.white);
-        ob = new Font("Algerian", Font.BOLD, 30);
     }
     public void paint(Graphics g)
     {
+        ob = new Font("Algerian", Font.BOLD, 30);
         g.setFont(ob);
-        g.drawString("Soumodip Das", 100, 200);
+        g.drawString("Santonu1",100,  50);
+        g.drawString("Soumodip Das",100,  100);
+        ob1 = new Font("Elephant", Font.ITALIC, 40);
+        g.setFont(ob1);
+        g.drawString("Soumodip", 100, 150);
+        g.drawString("Santonu2", 100, 200);
+        ob2 = new Font("Arial", 100, 40);
+        g.setFont(ob2);
+        g.drawString("Prodip", 100, 250);
     }
 }
