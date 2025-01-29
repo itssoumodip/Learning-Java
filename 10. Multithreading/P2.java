21c352e1787b3df8b00858a64b3a865afd56f279
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-29 07:42:10 +0000

    threading with priority

--- 10. Multithreading/P2.java
+++ 10. Multithreading/P2.java
@@ -1,7 +1,7 @@
-class P0 implements Runnable 
+class P1 implements Runnable 
 {
     Thread t;
-    public P0(String s, int n)
+    public P1(String s, int n)
     {
         t = new Thread(this, s);
         t.setPriority(n);
@@ -12,16 +12,22 @@ class P0 implements Runnable
         for (int i=0; i<11; i++)
         {
             String s1 = t.getName();
+            try
+            {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e)
+            {}
             System.out.println("Thread Name : "+s1+" Value : "+i+" Priority : "+t.getPriority());
         }
     }
 }
-class P1 
+class P2 
 {
     public static void main(String soumo[])
     {
-        P0 ob1 = new P0 ("santonu", 5);
-        P0 ob2 = new P0 ("soumodip", 10); 
-        P0 ob3 = new P0 ("prodip", 1); 
+        P1 ob1 = new P1 ("santonu", 5);
+        P1 ob2 = new P1 ("soumodip", 10); 
+        P1 ob3 = new P1 ("prodip", 1); 
     }
 }
