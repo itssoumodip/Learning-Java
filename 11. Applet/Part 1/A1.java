6472264172903cc6f5c69fbd35ed61931fba8931
itssoumodip <soumodipddas2005@gmail.com>
2025-01-29 17:04:44 +0530

    applet codes

--- 11. Applet/Part 1/A1.java
+++ 11. Applet/Part 1/A1.java
@@ -1,3 +1,19 @@
-public class A1 {
-    
+import java.applet.*;
+import java.awt.*;
+
+public class A1 extends Applet
+{
+    Font ob, ob1, ob2;
+
+    public void init()
+    {
+        setBackground(Color.black);
+        setForeground(Color.white);
+        ob = new Font("Algerian", Font.BOLD, 30);
+    }
+    public void paint(Graphics g)
+    {
+        g.setFont(ob);
+        g.drawString("Soumodip Das", 100, 200);
+    }
 }
