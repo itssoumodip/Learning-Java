24d2d6d921bdb2024716a85df4cc217cce6e0618
itssoumodip <soumodipddas2005@gmail.com>
2025-01-29 15:53:18 +0530

    about the applet part 1

---
+++ 11. Applet/Part 1/First.java
@@ -0,0 +1,33 @@
+import java.applet.*;
+import java.awt.*;                   
+//<applet code=First width =500 height=500></applet>
+public class First extends Applet
+{
+  Font ob,ob1,ob2;
+  public void init()
+  {
+     setBackground(Color.black);
+     setForeground(Color.white);
+     ob=new Font("algerian",Font.BOLD,30);
+  }
+  public void paint(Graphics g)
+  {
+     g.setFont(ob);
+     g.drawString("Soumodip Das",100,200);
+     ob1=new Font("bookman old style",Font.ITALIC,50); 
+     g.setFont(ob1);
+     g.setColor(Color.red);
+     g.drawString("Soumodip Das Chagol",100,300);
+    g.setColor(Color.yellow);
+    ob2=new Font("monotype corsiva",Font.BOLD,80); 
+    g.setFont(ob2);
+    g.drawString("Soumodip jali Programmer",50,600);
+  }
+}
+
+
+
+
+
+
+
