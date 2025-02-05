6e21290005d34829c80e9bdb5901a7706fc20861
itssoumodip <soumodipddas2005@gmail.com>
2025-02-05 21:26:56 +0530

    about the colour in applet

---
+++ 11. Applet/Part 1/Colour1.java
@@ -0,0 +1,36 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+// <applet code = Colour1 height = 500 width = 700></applet>
+
+public class Colour1 extends Applet implements ActionListener
+{
+    TextField obT1, obT2, obT3;
+    Button obB;
+    public void init()
+    {
+        obT1 = new TextField(10);
+        obT2 = new TextField(10);
+        obT3 = new TextField(10);
+        obB = new Button("Genarate Colour");
+        obT1.setText("0");
+        obT2.setText("0");
+        obT3.setText("0");
+        add(obT1);
+        add(obT2);
+        add(obT3);
+        add(obB);
+    }
+    public void actionPerformed (ActionEvent e)
+    {
+        repaint();
+    }
+    public void paint(Graphics g)
+    {
+        int x = Integer.parseInt(obT1.getText());
+        int y = Integer.parseInt(obT2.getText());
+        int z = Integer.parseInt(obT3.getText());
+        if (x>255 || y>255 || z>255 || x<0 || y<0 ||)
+    }
+}
