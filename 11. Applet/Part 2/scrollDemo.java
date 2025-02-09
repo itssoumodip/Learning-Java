dd14d8b980cf37465061422ec1d156a58e3791b4
itssoumodip <soumodipddas2005@gmail.com>
2025-02-09 17:43:31 +0530

    scrollDemo applet

--- 11. Applet/Part 2/scrollDemo.java
+++ 11. Applet/Part 2/scrollDemo.java
@@ -7,15 +7,35 @@ import java.applet.*;
 public class scrollDemo extends Applet implements AdjustmentListener, MouseMotionListener {
     String msg1 = "";
     String msg2 = "";
-    Scrollbar varticalSc, horizantalSc;
+    Scrollbar verticalSc, horizantalSc;
     public void init () {
-        varticalSc = new Scrollbar(Scrollbar.VARTICAL,0,0,0,300);
-        horizantalSc = new Scrollbar(Scrollbar.HORIZONTAL, 0,0,0,300);
-        add(varticalSc);
+        verticalSc = new Scrollbar(Scrollbar.VERTICAL, 0, 0, 0, 300);
+        horizantalSc = new Scrollbar(Scrollbar.HORIZONTAL, 0,0,0,400);
+        add(verticalSc);
         add(horizantalSc);
-        varticalSc.addAdjustmentListener(this);
+        verticalSc.addAdjustmentListener(this);
         horizantalSc.addAdjustmentListener(this);
-        addMouseListener(this);
+        addMouseMotionListener(this);
     }
-    public void  
+    public void adjustmentValueChanged(AdjustmentEvent ae) {
+        repaint();
+    }
+    public void mouseDragged(MouseEvent me) {
+        int x = me.getX();
+        int y = me.getY();
+        horizantalSc.setValue(x);
+        verticalSc.setValue(y);
+        repaint();
+    }
+    public void mouseMoved(MouseEvent me) {
+    }
+    public void paint(Graphics g) {
+        msg1 = "Vartical : "+ verticalSc.getValue();
+        msg2 = "Horizontal : "+ horizantalSc.getValue();
+        g.drawString(msg1, 6, 100);
+        g.drawString(msg2, 6, 150);
+        Font obF = new Font("Arial", Font.BOLD, 50);
+        g.setFont(obF);
+        g.drawString("Soumodip Das", horizantalSc.getValue(), verticalSc.getValue());
+    }
 }
