a3e07ef0baf88f644c5112a961a787ac5b0b8cf9
itssoumodip <soumodipddas2005@gmail.com>
2025-02-14 13:48:35 +0530

    making the multi media using applet

---
+++ 11. Applet/Part 4/multiMedia.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class multiMedia extends Frame
+{
+    String menuSelection = "Select a menu Item";
+
+    public static void main(Sting soumo[]) 
+    {
+        Multimedia ma = new Multimedia();
+    }    
+
+    public MultiMedia () 
+    {
+        super("MultiMedia")
+    }
+}
